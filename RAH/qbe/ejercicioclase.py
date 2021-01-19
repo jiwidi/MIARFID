4fdb719b402293f5769f91fff17ccf3cdbb4589d
Jaime <fhjaime96@gmail.com>
2021-01-19 14:55:27 +0100

    RAH QBE

--- RAH/qbe/ejercicioclase.py
+++ RAH/qbe/ejercicioclase.py
@@ -7,6 +7,7 @@ from matplotlib.pyplot import matshow, show, cm, plot
 import matplotlib.pyplot as plt
 import math
 from tabulate import tabulate
+from scipy.signal import argrelextrema
 
 
 def lee_fichero(fichero):
@@ -19,76 +20,85 @@ def lee_fichero(fichero):
 
 
 def stdw(distancias):
-    matrix = np.zeros(shape=(len(npmatriz1), len(npmatrix2), 3))
+    matrix = np.zeros(shape=(len(npmatrix1), len(npmatrix2), 4))
     # Calculamos T i,0
-    for i in range(1, len(npmatriz1)):
-        matrix[i][0] = [
-            distancias[i, 0] + matrix[i - 1][0][0],
-            matrix[i - 1][0][1] + 1,
-            matrix[i - 1][0][2],
-        ]
     # Calculamos T 0,j
     for j in range(len(npmatrix2)):
-        matrix[0][j] = [distancias[0, j], 0, j]
+        matrix[0][j] = [distancias[0, j], 1, j, distancias[0, j]]
+
+    for i in range(1, len(npmatrix1)):
+        matrix[i][0] = [
+            distancias[i, 0] + matrix[i - 1][0][0],
+            i,
+            0,
+            distancias[i, 0] + matrix[i - 1][0][0] / i,
+        ]
+
     # Calculamos T i,j
-    result = [0, 0, 0]
     for j in range(1, len(npmatrix2)):
-        for i in range(1, len(npmatriz1)):
-            aux1 = (matrix[i - 1][j - 1][0] + distancias[i, j]) / (
+        for i in range(1, len(npmatrix1)):
+            aux1 = (matrix[i - 1][j][0] + distancias[i][j]) / (matrix[i - 1][j][1] + 1)
+            aux2 = (matrix[i][j - 1][0] + distancias[i][j]) / (matrix[i][j - 1][1] + 1)
+            aux3 = (matrix[i - 1][j - 1][0] + distancias[i][j]) / (
                 matrix[i - 1][j - 1][1] + 1
             )
-            aux2 = (matrix[i - 1][j][0] + distancias[i, j]) / (matrix[i - 1][j][1] + 1)
-            aux3 = (matrix[i][j - 1][0] + distancias[i, j]) / (matrix[i][j - 1][1] + 1)
-            options = [[aux1, i - 1, j - 1], [aux2, i - 1, j], [aux3, i, j - 1]]
-            aux = [options[0][0], options[1][0], options[2][0]]
-            index_min = aux.index(min(aux))
-            result[0] = (
-                matrix[options[index_min][1]][options[index_min][2]][0]
-                + distancias[i, j]
-            )
-            result[1] = matrix[options[index_min][1]][options[index_min][2]][1] + 1
-            result[2] = matrix[options[index_min][1]][options[index_min][2]][2]
 
-            matrix[i][j] = result
+            aux = min(aux1, aux2, aux3)
 
-    last_rows = [matrix[len(npmatriz1) - 1][j] for j in range(len(npmatrix2))]
-    return last_rows
+            if aux == aux1:
+                matrix[i][j][0] = matrix[i - 1][j][0] + distancias[i][j]
+                matrix[i][j][2] = matrix[i - 1][j][2]
+            elif aux == aux2:
+                matrix[i][j][0] = matrix[i][j - 1][0] + distancias[i][j]
+                matrix[i][j][2] = matrix[i][j - 1][2]
+            else:
+                matrix[i][j][0] = matrix[i - 1][j - 1][0] + distancias[i][j]
+                matrix[i][j][2] = matrix[i - 1][j - 1][2]
+
+            matrix[i][j][1] = i
+            matrix[i][j][3] = matrix[i][j][0] / matrix[i][j][1]
+
+    last_rows = np.array(
+        [matrix[len(npmatrix1) - 1][j][3] for j in range(1, len(npmatrix2))]
+    )
+    starts = np.array(
+        [matrix[len(npmatrix1) - 1][j][2] for j in range(1, len(npmatrix2))]
+    )
+    return last_rows, starts
 
 
 if __name__ == "__main__":
-    npmatriz1 = lee_fichero("mfc_queries/SEG-0062.mfc.raw")
+    npmatrix1 = lee_fichero("mfc_queries/SEG-0032.mfc.raw")
     npmatrix2 = lee_fichero("largo250000.mfc.raw")
 
     print("Calculando distancias", end="\r")
-    distancias = get_distance_matrix(npmatriz1, npmatrix2, "cos")
+    distancias = get_distance_matrix(npmatrix1, npmatrix2, "cos")
     print("Calculando distancias ... DONE")
 
     print("Calculando stdw", end="\r")
-    last_row = stdw(distancias)
+    last_row, starts = stdw(distancias)
     print("Calculando stdw ... DONE")
 
     ventana = 100
-    print(f"Encontrando minimos locales con ventana {ventana}", end="\r")
-    minimos = []
-    npmatrix2_length = int(round(len(npmatrix2)))
-    for index in range(0, npmatrix2_length, ventana):
-        minimun_cost = math.inf
-        minimo = None
-        for x in range(index + 1, index + ventana):
-            if last_row[x][0] < minimun_cost:
-                minimun_cost = last_row[x][0]
-                minimo = last_row[x]
-        minimos.append(minimo)
+    print()
+    print(f"Encontrando minimos locales", end="\r")
+    min_index = argrelextrema(last_row, np.less)[0]
+    minimos = np.take(last_row, min_index).tolist()
+    starts = np.take(starts, min_index).tolist()
 
-    minimos.sort(key=lambda x: x[0])
-    minimos_result = [
-        [
-            str(minimos[i][2] / 100),
-            str((minimos[i][2] + minimos[i][1]) / 100),
-            str(minimos[i][0]),
-        ]
-        for i in range(10)
-    ]
+    _, starts = zip(*sorted(zip(minimos, starts)))
+    minimos, min_index = zip(*sorted(zip(minimos, min_index)))
+
+    # minimos_result = [[starts[i], min_index[i], minimos[i]] for i in range(10)]
+    visited_starts = []
+    minimos_result = []
+    for i in range(len(minimos)):
+        if starts[i] not in visited_starts:
+            minimos_result.append([starts[i], min_index[i], minimos[i]])
+            visited_starts.append(starts[i])
+        if len(visited_starts) == 10:
+            break
+
     print("Encontrando minimos ... DONE")
     print("TOP 10")
     print(
