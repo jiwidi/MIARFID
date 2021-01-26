83a084b4ba6c13412dd046d9c14b6e1d8acbedc2
FERRANDO HUERTAS <jaiferhu@EVIRL-034-OK.dsic.cloud>
2021-01-26 17:43:21 +0100

    prepare data moses

--- TA/Proyecto/moses/prepare_data.sh
+++ TA/Proyecto/moses/prepare_data.sh
@@ -8,55 +8,67 @@ tar zxvf Corpus.tgz
 
 echo "Cleaning corpus"
 
-#TRAINING
-##EN
-$MOSES/scripts/tokenizer/tokenizer.perl -l en < Corpus/europarl-v7.es-en-train-red.en > Corpus/train.tok.en
-$MOSES/scripts/tokenizer/lowercase.perl < Corpus/train.tok.en > Corpus/train.tok.lc.en
-##ES
-$MOSES/scripts/tokenizer/tokenizer.perl -l es < Corpus/europarl-v7.es-en-train-red.es > Corpus/train.tok.es
-$MOSES/scripts/tokenizer/lowercase.perl < Corpus/train.tok.es > Corpus/train.tok.lc.es
+# #TRAINING
+# ##EN
+# $MOSES/scripts/tokenizer/tokenizer.perl -l en < Corpus/europarl-v7.es-en-train-red.en > Corpus/train.tok.en
+# $MOSES/scripts/tokenizer/lowercase.perl < Corpus/train.tok.en > Corpus/train.tok.lc.en
+# ##ES
+# $MOSES/scripts/tokenizer/tokenizer.perl -l es < Corpus/europarl-v7.es-en-train-red.es > Corpus/train.tok.es
+# $MOSES/scripts/tokenizer/lowercase.perl < Corpus/train.tok.es > Corpus/train.tok.lc.es
 
-clean-corpus-n.perl Corpus/train.tok.lc es en Corpus/training.clean 1 60
+# clean-corpus-n.perl Corpus/train.tok.lc es en Corpus/training.clean 1 60
 
-#Test
-##EN
-$MOSES/scripts/tokenizer/tokenizer.perl -l en < Corpus/europarl-v7.es-en-test.en > Corpus/test.tok.en
-$MOSES/scripts/tokenizer/lowercase.perl < Corpus/test.tok.en > Corpus/test.tok.lc.en
-##ES
-$MOSES/scripts/tokenizer/tokenizer.perl -l es < Corpus/europarl-v7.es-en-test.es > Corpus/test.tok.es
-$MOSES/scripts/tokenizer/lowercase.perl < Corpus/test.tok.es > Corpus/test.tok.lc.es
+# #Test
+# ##EN
+# $MOSES/scripts/tokenizer/tokenizer.perl -l en < Corpus/europarl-v7.es-en-test.en > Corpus/test.tok.en
+# $MOSES/scripts/tokenizer/lowercase.perl < Corpus/test.tok.en > Corpus/test.tok.lc.en
+# ##ES
+# $MOSES/scripts/tokenizer/tokenizer.perl -l es < Corpus/europarl-v7.es-en-test.es > Corpus/test.tok.es
+# $MOSES/scripts/tokenizer/lowercase.perl < Corpus/test.tok.es > Corpus/test.tok.lc.es
 
-clean-corpus-n.perl Corpus/test.tok.lc es en Corpus/test.clean 1 60
+# clean-corpus-n.perl Corpus/test.tok.lc es en Corpus/test.clean 1 60
 
-mkdir data; cd data
-mkdir dataset
+# mkdir data; cd data
+# mkdir dataset
 
-mv ../Corpus/training.clean.es dataset/tr-full.src
-mv ../Corpus/training.clean.en dataset/tr-full.tgt
+# mv ../Corpus/training.clean.es dataset/tr-full.src
+# mv ../Corpus/training.clean.en dataset/tr-full.tgt
 
-mv ../Corpus/test.clean.es dataset/test.src
-mv ../Corpus/test.clean.en dataset/test.tgt
+# mv ../Corpus/test.clean.es dataset/test.src
+# mv ../Corpus/test.clean.en dataset/test.tgt
+
+# cd ..
+
+# python3 split_train.py
+
+export LANGUAGE=C 
+export LC_ALL=C 
+
+cd Corpus
+cp europarl-v7.es-en-train-red.en train_tmp.en
+cp europarl-v7.es-en-train-red.es train_tmp.es
+cp europarl-v7.es-en-test.en test_tmp.en
+cp europarl-v7.es-en-test.es test_tmp.es
+for DATA in train_tmp test_tmp
+do
+    for LANG in en es
+        do
+            $MOSES/scripts/tokenizer/tokenizer.perl -l $LANG < "$DATA.$LANG" > $DATA.tk.$LANG
+            $MOSES/scripts/tokenizer/lowercase.perl < $DATA.tk.$LANG > $DATA.tk.lc.$LANG
+        done
+        clean-corpus-n.perl $DATA.tk.lc es en ${DATA%_*}.clean 1 60
+done
+
+rm *_tmp*
+
+
+mv train.clean.es ../data/dataset/tr-full.src
+mv train.clean.en ../data/dataset/tr-full.tgt
+
+mv test.clean.es ../data/dataset/test.src
+mv test.clean.en ../data/dataset/test.tgt
 
 cd ..
-
+echo "Removing duplicate lines and dividing train data in train/development sets..."
 python3 split_train.py
-
-
-# cp europarl-v7.es-en-train-red.en train_tmp.en
-# cp europarl-v7.es-en-train-red.es train_tmp.es
-# cp europarl-v7.es-en-test.en test_tmp.en
-# cp europarl-v7.es-en-test.es test_tmp.es
-# for DATA in train_tmp test_tmp 15
-# do
-#     for LANG in en es
-#         do
-#             $MOSES/scripts/tokenizer/tokenizer.perl -l $LANG < "$DATA.$LANG" > $DATA.tk.$LANG
-#             $MOSES/scripts/tokenizer/lowercase.perl < $DATA.tk.$LANG > $DATA.tk.lc.$LANG
-#         done
-#         clean-corpus-n.perl $DATA.tk.lc es en ${DATA%_*}.clean 1 60
-# done
-
-# rm *_tmp*
-
-# echo "Removing duplicate lines and dividing train data in train/development sets..."
 
--- TA/Proyecto/moses/split_train.py
+++ TA/Proyecto/moses/split_train.py
@@ -1,14 +1,14 @@
 import pandas as pd 
 
 
-print("Procesando train EN")
+print("Procesando train ES")
 train_en = open('data/dataset/tr.tgt', 'w')
 dev_en = open('data/dataset/dev.tgt', 'w')
 
 i=0
 with open('data/dataset/tr-full.tgt') as f:
     for line in f.readlines():
-        if i<42000:
+        if i<43000:
             train_en.write(line)
         else:
             dev_en.write(line)
@@ -19,13 +19,13 @@ train_en.close()
 dev_en.close()
 
 
-print("Procesando train ES")
+print("Procesando train EN")
 dev_es = open('data/dataset/dev.src', 'w')
 train_es = open('data/dataset/tr.src', 'w')
 i=0
 with open('data/dataset/tr-full.src') as f:
     for line in f.readlines():
-        if i<42000:
+        if i<43000:
             train_es.write(line)
         else:
             dev_es.write(line)
