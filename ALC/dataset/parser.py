7e0f6d25a7fc210e80fa4f6a2c373b390fbee3c7
Jaime Ferrando Huertas <fhjaime96@gmail.com>
2021-04-14 15:41:49 +0200

    Update parser.py

--- ALC/dataset/parser.py
+++ ALC/dataset/parser.py
@@ -4,9 +4,14 @@ import os
 
 
 
-root_folder = 'pan21-author-profiling-training-2021-03-14/en/'
+root_folder = 'dataset/pan21-author-profiling-training-2021-03-14/en/'
 
 
+truths={}
+with open(root_folder+"truth.txt") as f:
+    for line in f:
+        author, tag = line.split(":::")
+        truths[author]=tag
 
 data_en = []
 for file in os.listdir(root_folder):
@@ -15,10 +20,10 @@ for file in os.listdir(root_folder):
         assert root[0].tag=="documents"
         for child in root[0]:
             assert child.tag=="document"
-            data = [file[:-4], child.text] # [userid, tweettext]
+            data = [file[:-4], child.text, tag] # [userid, tweettext,tag ]
             data_en.append(data)
 
-root_folder = 'pan21-author-profiling-training-2021-03-14/en/'
+root_folder = 'dataset/pan21-author-profiling-training-2021-03-14/en/'
 data_es = []
 for file in os.listdir(root_folder):
     if file[-3:]=="xml":
@@ -28,5 +33,10 @@ for file in os.listdir(root_folder):
             assert child.tag=="document"
             data = [file[:-4], child.text] # [userid, tweettext]
             data_es.append(data)
+
+
+data_en = pd.DataFrame(data_en, columns=['author_id','tweet','tag'])
+data_en.to_csv("dataset/data_en.csv",index=False)
+print(data_en)
 
 
