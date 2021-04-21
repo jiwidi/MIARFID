ed0a3b64c19a449f47a218a2a9912cd0e83dc3e9
Jaime <fhjaime96@gmail.com>
2021-04-21 20:49:00 +0200

    new datasets with fixed tag

--- ALC/paolo/parser.py
+++ ALC/paolo/parser.py
@@ -6,7 +6,7 @@ import os
 
 root_folder = 'dataset/pan21-author-profiling-training-2021-03-14/en/'
 
-
+print("Processing")
 truths={}
 with open(root_folder+"truth.txt") as f:
     for line in f:
@@ -20,7 +20,7 @@ for file in os.listdir(root_folder):
         assert root[0].tag=="documents"
         for child in root[0]:
             assert child.tag=="document"
-            data = [file[:-4], child.text, tag] # [userid, tweettext,tag ]
+            data = [file[:-4], child.text, truths[file[:-4]]] # [userid, tweettext,tag ]
             data_en.append(data)
 
 root_folder = 'dataset/pan21-author-profiling-training-2021-03-14/en/'
@@ -31,12 +31,16 @@ for file in os.listdir(root_folder):
         assert root[0].tag=="documents"
         for child in root[0]:
             assert child.tag=="document"
-            data = [file[:-4], child.text] # [userid, tweettext]
+            data = [file[:-4], child.text,truths[file[:-4]]] # [userid, tweettext]
             data_es.append(data)
 
-
+print("Saving to files")
 data_en = pd.DataFrame(data_en, columns=['author_id','tweet','tag'])
 data_en.to_csv("dataset/data_en.csv",index=False)
-print(data_en)
+
+data_es = pd.DataFrame(data_en, columns=['author_id','tweet','tag'])
+data_es.to_csv("dataset/data_es.csv",index=False)
+
+print("Done")
 
 
