b7e8d05aad2bc4750704c5557634b065734be2a3
jiwidi <fhjaime96@gmail.com>
2021-05-21 20:39:07 +0200

    ex6 single_script.sh

--- AAA/generate_triangle_data.py
+++ AAA/generate_triangle_data.py
@@ -3,11 +3,8 @@ from tqdm import tqdm
 import subprocess
 
 # Equil
-raw_equil = open("DATA/Tr-equil").readlines()
-raw_isosc = open("DATA/Tr-isosc").readlines()
-raw_right = open("DATA/Tr-right").readlines()
 
-N = 1000  # number of extra samples
+# N = 1000  # number of extra samples
 MIN_LEN = 1
 MAX_LEN = 20
 SEED = 1
@@ -25,91 +22,118 @@ def check_triangle(a, b, c):
         return False
 
 
-print(f"Equil data has {len(raw_equil)} samples")
-aux_triangles = (
-    subprocess.check_output(
-        [
-            "scfg-toolkit/genFig",
-            "-F",
-            "1",
-            "-s",
-            str(SEED),
-            "-c",
-            str(N),
-            "-l",
-            str(MIN_LEN),
-            "-L",
-            str(MAX_LEN),
-        ]
+def gen_data(n):
+    raw_equil = open("DATA/Tr-equil").readlines()
+    raw_isosc = open("DATA/Tr-isosc").readlines()
+    raw_right = open("DATA/Tr-right").readlines()
+    print(f"Equil data has {len(raw_equil)} samples")
+    aux_triangles = (
+        subprocess.check_output(
+            [
+                "scfg-toolkit/genFig",
+                "-F",
+                "1",
+                "-s",
+                str(SEED),
+                "-c",
+                str(n),
+                "-l",
+                str(MIN_LEN),
+                "-L",
+                str(MAX_LEN),
+            ]
+        )
+        .decode("utf-8")
+        .split("\n")
     )
-    .decode("utf-8")
-    .split("\n")
-)
-aux_triangles.pop()  # Remove last empty triangle
+    aux_triangles.pop()  # Remove last empty triangle
 
-raw_equil = raw_equil + aux_triangles
-with open("DATA/tr-equil-extended", "w+") as f:
-    for element in raw_equil:
-        f.write(element.replace("\n", "") + "\n")
-print(f"Equil data has {len(raw_equil)} samples now")
+    raw_equil = raw_equil + aux_triangles
+    with open(f"DATA/Tr-equil-extended{n}", "w+") as f:
+        for element in raw_equil:
+            f.write(element.replace("\n", "") + "\n")
+    print(f"Equil data has {len(raw_equil)} samples now")
 
-
-print(f"Isosc data has {len(raw_isosc)} samples")
-# Isosceles
-aux_triangles = (
-    subprocess.check_output(
-        [
-            "scfg-toolkit/genFig",
-            "-F",
-            "2",
-            "-s",
-            str(SEED),
-            "-c",
-            str(N),
-            "-l",
-            str(MIN_LEN),
-            "-L",
-            str(MAX_LEN),
-        ]
+    print(f"Isosc data has {len(raw_isosc)} samples")
+    # Isosceles
+    aux_triangles = (
+        subprocess.check_output(
+            [
+                "scfg-toolkit/genFig",
+                "-F",
+                "2",
+                "-s",
+                str(SEED),
+                "-c",
+                str(n),
+                "-l",
+                str(MIN_LEN),
+                "-L",
+                str(MAX_LEN),
+            ]
+        )
+        .decode("utf-8")
+        .split("\n")
     )
-    .decode("utf-8")
-    .split("\n")
-)
-aux_triangles.pop()  # Remove last empty triangle
-raw_isosc = raw_isosc + aux_triangles
-with open("DATA/tr-isosc-extended", "w+") as f:
-    for element in raw_isosc:
-        f.write(element.replace("\n", "") + "\n")
-print(f"Isosc data has {len(raw_isosc)} samples now")
+    aux_triangles.pop()  # Remove last empty triangle
+    raw_isosc = raw_isosc + aux_triangles
+    with open(f"DATA/Tr-isosc-extended{n}", "w+") as f:
+        for element in raw_isosc:
+            f.write(element.replace("\n", "") + "\n")
+    print(f"Isosc data has {len(raw_isosc)} samples now")
 
-# RIGHT
-print(f"Isosc data has {len(raw_right)} samples")
-# Isosceles
-aux_triangles = (
-    subprocess.check_output(
-        [
-            "scfg-toolkit/genFig",
-            "-F",
-            "0",
-            "-s",
-            str(SEED),
-            "-c",
-            str(N),
-            "-l",
-            str(MIN_LEN),
-            "-L",
-            str(MAX_LEN),
-        ]
+    # RIGHT
+    print(f"Isosc data has {len(raw_right)} samples")
+    # Isosceles
+    aux_triangles = (
+        subprocess.check_output(
+            [
+                "scfg-toolkit/genFig",
+                "-F",
+                "0",
+                "-s",
+                str(SEED),
+                "-c",
+                str(n),
+                "-l",
+                str(MIN_LEN),
+                "-L",
+                str(MAX_LEN),
+            ]
+        )
+        .decode("utf-8")
+        .split("\n")
     )
-    .decode("utf-8")
-    .split("\n")
-)
-aux_triangles.pop()  # Remove last empty triangle
+    aux_triangles.pop()  # Remove last empty triangle
 
-raw_right = raw_right + aux_triangles
-with open("DATA/tr-right-extended", "w+") as f:
-    for element in raw_right:
+    raw_right = raw_right + aux_triangles
+    with open(f"DATA/Tr-right-extended-{n}", "w+") as f:
+        for element in raw_right:
+            f.write(element.replace("\n", "") + "\n")
+    print(f"Isosc data has {len(raw_right)} samples now")
 
-        f.write(element.replace("\n", "") + "\n")
-print(f"Isosc data has {len(raw_right)} samples now")
+    print("Creating negative files")
+
+    raw_equil = open("DATA/Tr-equil").readlines()
+    raw_isosc = open("DATA/Tr-isosc").readlines()
+    raw_right = open("DATA/Tr-right").readlines()
+    with open("DATA/tr-right-neg", "w+") as f:
+        for element in raw_isosc:
+            f.write(element.replace("\n", "") + "\n")
+        for element in raw_equil:
+            f.write(element.replace("\n", "") + "\n")
+
+    with open("DATA/tr-equil-neg", "w+") as f:
+        for element in raw_right:
+            f.write(element.replace("\n", "") + "\n")
+        for element in raw_isosc:
+            f.write(element.replace("\n", "") + "\n")
+    with open("DATA/tr-isosc-neg", "w+") as f:
+        for element in raw_right:
+            f.write(element.replace("\n", "") + "\n")
+        for element in raw_equil:
+            f.write(element.replace("\n", "") + "\n")
+
+
+gen_data(2000)
 
--- AAA/single_script.sh
+++ AAA/single_script.sh
@@ -1,8 +1,7 @@
-
 # train models
-scfg-toolkit/scfg_learn_mmi -g MODELS/G-1 -f MODELS/right-0.10 -p DATA/Tr-right -n DATA/Tr-right-neg -i 1
-scfg-toolkit/scfg_learn_mmi -g MODELS/G-1 -f MODELS/equil-0.10 -p DATA/Tr-equil -n DATA/Tr-equil-neg -i 1
-scfg-toolkit/scfg_learn_mmi -g MODELS/G-1 -f MODELS/isosc-0.10 -p DATA/Tr-isosc -n DATA/Tr-isosc-neg -i 1
+scfg-toolkit/scfg_learn_mmi -g MODELS/G-1 -f MODELS/right-0.10 -p DATA/Tr-right -n DATA/tr-right-neg  -H 0.6 -i 1
+scfg-toolkit/scfg_learn_mmi -g MODELS/G-1 -f MODELS/equil-0.10 -p DATA/Tr-equil -n DATA/tr-equil-neg  -H 0.6 -i 1
+scfg-toolkit/scfg_learn_mmi -g MODELS/G-1 -f MODELS/isosc-0.10 -p DATA/Tr-isosc -n DATA/tr-isosc-neg  -H 0.0 -i 1
 # classify with the trained models and get results
 scfg-toolkit/scfg_prob -g MODELS/right-0.10 -m DATA/Ts-right > tmp/r
 scfg-toolkit/scfg_prob -g MODELS/equil-0.10 -m DATA/Ts-right > tmp/e
@@ -20,4 +19,3 @@ scfg-toolkit/scfg_prob -g MODELS/isosc-0.10 -m DATA/Ts-isosc > tmp/i
 paste tmp/r tmp/e tmp/i | awk '{ m=$3;argm="isosc"; if ($1>m) { m=$1;argm="right";} if ($2>m) { m=$2; argm="equil"; } printf("isosc %s\n",argm); }' >> results
 
 cat results | scfg-toolkit/confus
-
