6c3e45c6b82e4b2d4a2d466846929c2d471e87ba
jiwidi <fhjaime96@gmail.com>
2021-06-01 10:36:37 +0200

    Big updates

---
+++ CV/utils.py
@@ -0,0 +1,42 @@
+# import the necessary packages
+from collections import namedtuple
+import numpy as np
+
+# define the `Detection` object
+Detection = namedtuple("Detection", ["image_path", "gt", "pred"])
+
+
+def bb_intersection_over_union(boxA, boxB):
+    # determine the (x, y)-coordinates of the intersection rectangle
+    xA = max(boxA[0], boxB[0])
+    yA = max(boxA[1], boxB[1])
+    xB = min(boxA[2], boxB[2])
+    yB = min(boxA[3], boxB[3])
+
+    # compute the area of intersection rectangle
+    interArea = (xB - xA) * (yB - yA)
+
+    # compute the area of both the prediction and ground-truth
+    # rectangles
+    boxAArea = (boxA[2] - boxA[0]) * (boxA[3] - boxA[1])
+    boxBArea = (boxB[2] - boxB[0]) * (boxB[3] - boxB[1])
+
+    # compute the intersection over union by taking the intersection
+    # area and dividing it by the sum of prediction + ground-truth
+    # areas - the interesection area
+    iou = interArea / float(boxAArea + boxBArea - interArea)
+
+    # return the intersection over union value
+    return iou
+
+
+def output(inputsize, kernel_size, padding=0, stride=1, filtros="1"):
+    return f"{filtros}x{((inputsize + (2 * padding) - kernel_size) / stride) + 1}"
+
+
+def cost(channels, output_r, outpur_c, kernel_r, kernel_c, filtros):
+    return channels * output_r * outpur_c * kernel_r * kernel_c * filtros
+
+
+def parameters(channels, kernel_r, kernel_c, filtros):
+    return channels * kernel_r * kernel_c * filtros
