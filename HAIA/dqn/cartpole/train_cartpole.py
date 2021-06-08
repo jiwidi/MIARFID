c4b76b9c8b02c987d74b4629043c13015343c6ff
Jaime <fhjaime96@gmail.com>
2021-06-08 09:57:43 +0200

    Haia cartpole

--- HAIA/dqn/cartpole/train_cartpole.py
+++ HAIA/dqn/cartpole/train_cartpole.py
@@ -7,6 +7,7 @@ from torch import nn
 import torch.nn.functional as F
 import gym
 
+
 class DQN(nn.Module):
     def __init__(self, input_dim, output_dim):
         super(DQN, self).__init__()
@@ -14,7 +15,6 @@ class DQN(nn.Module):
         self.linear2 = nn.Linear(16, 32)
         self.linear3 = nn.Linear(32, 32)
         self.linear4 = nn.Linear(32, output_dim)
-
 
     def forward(self, x):
         x = F.relu(self.linear1(x))
@@ -33,8 +33,9 @@ steps_done = 0
 def select_action(state):
     global steps_done
     sample = random.random()
-    eps_threshold = final_epsilon + (initial_epsilon - final_epsilon) * \
-                    math.exp(-1. * steps_done / epsilon_decay)
+    eps_threshold = final_epsilon + (initial_epsilon - final_epsilon) * math.exp(
+        -1.0 * steps_done / epsilon_decay
+    )
     if sample > eps_threshold:
         with torch.no_grad():
             state = torch.Tensor(state)
@@ -43,26 +44,25 @@ def select_action(state):
             node_activated = int(torch.argmax(q_calc))
             return node_activated
     else:
-        node_activated = random.randint(0,1)
+        node_activated = random.randint(0, 1)
         steps_done += 1
         return node_activated
 
 
-class ReplayMemory(object): # Stores [state, reward, action, next_state, done]
-
+class ReplayMemory(object):  # Stores [state, reward, action, next_state, done]
     def __init__(self, capacity):
         self.capacity = capacity
-        self.memory = [[],[],[],[],[]]
+        self.memory = [[], [], [], [], []]
 
     def push(self, data):
         """Saves a transition."""
         for idx, point in enumerate(data):
-            #print("Col {} appended {}".format(idx, point))
+            # print("Col {} appended {}".format(idx, point))
             self.memory[idx].append(point)
 
     def sample(self, batch_size):
         rows = random.sample(range(0, len(self.memory[0])), batch_size)
-        experiences = [[],[],[],[],[]]
+        experiences = [[], [], [], [], []]
         for row in rows:
             for col in range(5):
                 experiences[col].append(self.memory[col][row])
@@ -106,8 +106,9 @@ def optimize_model():
             next_state_q_vals[idx] = -1
         else:
             # .max in pytorch returns (values, idx), we only want vals
-            next_state_q_vals[idx] = (target_net(next_state_batch[idx]).max(0)[0]).detach()
-
+            next_state_q_vals[idx] = (
+                target_net(next_state_batch[idx]).max(0)[0]
+            ).detach()
 
     better_pred = (reward_batch + next_state_q_vals).unsqueeze(1)
 
@@ -124,43 +125,49 @@ points = []
 losspoints = []
 
 
-env = gym.make('CartPole-v0')
+env = gym.make("CartPole-v0")
 for i_episode in range(5000):
     observation = env.reset()
     episode_loss = 0
-    solved=False
+    solved = False
     if i_episode % tau == 0:
         target_net.load_state_dict(model.state_dict())
     for t in range(1000):
-        #env.render()
+        # env.render()
         state = observation
         action = select_action(observation)
         observation, reward, done, _ = env.step(action)
 
         if done:
-            next_state = [0,0,0,0]
+            next_state = [0, 0, 0, 0]
         else:
             next_state = observation
 
         memory.push([state, action, reward, next_state, done])
         episode_loss = episode_loss + float(optimize_model())
         if done:
-            if(t+1==200):
-                print("Problem solved at Episode {} finished after {} timesteps".format(i_episode, t+1))
-                solved=True
+            points.append((i_episode, t + 1))
+            print("Episode {} finished after {} timesteps".format(i_episode, t + 1))
+            print("Avg Loss: ", episode_loss / (t + 1))
+            losspoints.append((i_episode, episode_loss / (t + 1)))
+            if t + 1 == 200:
+                print(
+                    "Problem solved at Episode {} finished after {} timesteps".format(
+                        i_episode, t + 1
+                    )
+                )
+                solved = True
                 break
-
-
-            points.append((i_episode, t+1))
-            print("Episode {} finished after {} timesteps".format(i_episode, t+1))
-            print("Avg Loss: ", episode_loss / (t+1))
-            losspoints.append((i_episode, episode_loss / (t+1)))
-            if (i_episode % 100 == 0):
-                eps = final_epsilon + (initial_epsilon - final_epsilon) * \
-                    math.exp(-1. * steps_done / epsilon_decay)
+            if i_episode % 100 == 0:
+                eps = final_epsilon + (initial_epsilon - final_epsilon) * math.exp(
+                    -1.0 * steps_done / epsilon_decay
+                )
                 print(eps)
-            if ((i_episode+1) % 5001 == 0):
-                save = {'state_dict': model.state_dict(), 'optimizer': optimizer.state_dict()}
+            if (i_episode + 1) % 5001 == 0:
+                save = {
+                    "state_dict": model.state_dict(),
+                    "optimizer": optimizer.state_dict(),
+                }
                 torch.save(save, "models/DQN_target_" + str(i_episode // 5000) + ".pth")
             break
     if solved:
@@ -168,12 +175,10 @@ for i_episode in range(5000):
 env.close()
 
 
-
-
 x = [coord[0] * 100 for coord in points]
 y = [coord[1] for coord in points]
 
 plt.plot(x, y)
-plt.xlabel('Steps', fontsize=18)
-plt.ylabel('Score', fontsize=16)
-plt.savefig('score_cartpole.png')
+plt.xlabel("Steps", fontsize=18)
+plt.ylabel("Score", fontsize=16)
+plt.savefig("score_cartpole.png")
